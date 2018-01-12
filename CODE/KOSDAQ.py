fae0cd5fa03e8169cf1b5c5b055fa7bc9afa8e59
Hyung Jun Sohn <jamesohn247@gmail.com>
2018-01-12 13:28:53 +0900

    data and portfolio code

---
+++ CODE/KOSDAQ.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import mylib as my
+matplotlib.rc('font', family='Malgun Gothic',size=8, weight = 'bold')
+
+KOSDAQ_Data = pd.ExcelFile('KOSDAQ2.xlsx', header = 2)
+KOSDAQ = pd.read_excel('KOSDAQ2.xlsx', sheetnames=0, header=1)
+KOSDAQ150 = pd.read_excel('KOSDAQ2.xlsx', sheetnames=1)
+국고채3년 = pd.read_excel('KOSDAQ2.xlsx', sheetname=2)
+코스닥인버스 = pd.read_excel('KOSDAQ2.xlsx', sheetname=3, header=1)
+금리 = pd.read_excel('KOSDAQ2.xlsx', sheetname=4, header=1)
+
+
+
+rf = (금리.국고5년/100)/365
+rf.index = KOSDAQ.날짜
+pf = my.누적수익곡선(KOSDAQ,0.015)
+
+def 누적수익곡선(데이터,매매비용=0.015,현금비중=4):
+    누적수익 = 1
+    gain = my.일간수익률(데이터,매매비용)*현금비중/4
+    bond = rf.values*현금비중/4
+    누적수익 = (1+gain+bond).cumprod()
+    누적수익.index = 데이터.날짜
+    return 누적수익
+
+
+a = my.벤치마크(KOSDAQ)
+b = my.누적수익곡선(KOSDAQ,0.015,my.MOM(KOSDAQ))
+c = 누적수익곡선(KOSDAQ,0.015,my.MOM(KOSDAQ))
+d = pd.concat([a, b, c], axis=1).dropna()
+d.columns = ['벤치마크(코스닥)', '현금혼합종가베팅', '국고채혼합종가베팅_5년']
+d.divide(d.ix[0]).plot(figsize = (18,12))
+plt.show()
+
+최대하락 = d.국고채혼합종가베팅_5년.rolling(min_periods=1, window = 500).max()
+당월하락 = d.국고채혼합종가베팅_5년/최대하락 - 1.0
+최대하락폭 = 당월하락.rolling(min_periods=1, window=500).min()
+
+당월하락.plot(subplots=True, figsize = (18,2), linestyle='dotted')
+최대하락폭.plot(subplots=True, figsize = (18,2), color = 'red', linestyle='dotted')
+plt.show()
+
+#6. MDD / CAGR
+투자기간 = len(d.index)/262
+print("MDD : "+str(최대하락폭.min()*100)[0:5]+"%")
+print("CAGR : "+str(d.국고채혼합종가베팅_5년[-1]**(1/투자기간)*100-100)[0:4]+"%")
---
+++ CODE/mylib.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.rc('font', family='Malgun Gothic',size=8, weight = 'bold')
+
+KOSDAQ_Data = pd.ExcelFile('KOSDAQ2.xlsx', header = 2)
+KOSDAQ = pd.read_excel('KOSDAQ2.xlsx', sheetnames=0, header=1)
+KOSDAQ150 = pd.read_excel('KOSDAQ2.xlsx', sheetnames=1)
+
+
+# 함수정의
+
+# 승리한 경우의 전일 종가 당일 시가
+def Gap_Win(데이터):
+    return 데이터[데이터.시가지수 > 데이터.종가지수.shift(1)]
+
+# 기간 내 종가베팅 승률
+def Win_Rate(데이터):
+    return len(Gap_Win(데이터)) / len(데이터)
+
+
+# 연단위 승률
+def 일간수익률(데이터,매매비용=0.015):
+    return (데이터.시가지수*(1-매매비용/100) - 데이터.종가지수.shift(1)*(1+매매비용/100)) / (데이터.종가지수.shift(1)*(1+매매비용/100))
+
+def Win_Rate_Graph(데이터):
+    x = []
+    y = []
+    for i in range(0,18):
+        start = i*261
+        end = start + 261
+        y = y + [Win_Rate(데이터[start:end])]
+        x.append(i)
+    print(x)
+    print(y)
+    plt.plot(x,y)
+    plt.show()
+
+def MOM(데이터):
+    a = 데이터.종가지수>데이터['3일 평균']
+    b = 데이터.종가지수>데이터['5일 평균']
+    c = 데이터.종가지수>데이터['10일 평균']
+    d = 데이터.종가지수>데이터['15일 평균']
+    k = [sum(x) for x in zip(a,b,c,d)]
+    return k
+
+def 벤치마크(데이터):
+    a = pd.DataFrame(데이터.종가지수 / 데이터.종가지수.shift(1))
+    a.index = 데이터.날짜
+    c = a.cumprod()
+    return c
+
+
+def 누적수익곡선(데이터,매매비용=0.015,현금비중=4):
+    누적수익 = 1
+    gain = 일간수익률(데이터,매매비용)*현금비중/4
+    누적수익 = (1+gain).cumprod()
+    누적수익.index = 데이터.날짜
+    return 누적수익
+
+def SR(포트폴리오수익률, 무위험이자):
+    pf = 포트폴리오수익률
+    rf = 무위험이자
+    ex = pf-rf
+    sr = ex.cumprod()/pf.std()
+
+    return sr
+
+
+## MDD
+
